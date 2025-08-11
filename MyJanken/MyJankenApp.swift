f789981be167404393d2f9ea5ec821f2b6a8cf40
Takahiro Kakisu <takahirokakisu@gmail.com>
2025-08-11 16:16:41 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by takahiro kakisu on 2025/08/11.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
